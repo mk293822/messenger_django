3b9ab950e2509671c41502829067aefbceacb1ee
Minkhant <mkt293822@gmail.com>
2024-11-14 11:43:34 +0630

    add side bar but not working css

---
+++ Messenger/static/js/script.js
@@ -0,0 +1,6 @@
+ // JavaScript to toggle sidebar visibility
+      function toggleSidebar() {
+        var sidebar = document.getElementById("sidebar");
+        sidebar.classList.toggle("active");// Toggle 'active' class to show/hide sidebar
+        console.log('Hello world');
+      }
--- project/settings.py
+++ project/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
 MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
